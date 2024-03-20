dbcb1df978f3c8c1aa962f36891e7387d5dd3654
Esraa Abduallah <esraahisham753@gmail.com>
2024-03-20 16:13:21 +0000

    Add logic for adding items

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@ import TodoList from "./todolist.js";
 import ToDoItem from "./todoitem.js";
 
 let todolist = new TodoList();
+const inputEl = document.getElementById("itemInput");
 
 document.addEventListener("readystatechange", function (event) {
   if (event.target.readyState == "complete") {
@@ -10,12 +11,20 @@ document.addEventListener("readystatechange", function (event) {
 });
 
 const initApp = () => {
+  const form = document.getElementById("addItem");
+  form.addEventListener("submit", (event) => {
+    event.preventDefault();
+    processSubmit();
+  });
+  
   refreshThePage();
 };
 
 const refreshThePage = () => {
   clearListDisplay();
   renderItems();
+  clearInput();
+  returnFocus();
 };
 
 const clearListDisplay = () => {
@@ -41,7 +50,7 @@ const buildTheItem = (item) => {
   const li = document.createElement("li");
   li.className = "list-container__item";
   const checkbox = document.createElement("input");
-  checkbox.type = "check";
+  checkbox.type = "checkbox";
   checkbox.id = item.getid();
   checkbox.tabIndex = "0";
   addCheckEventListener(checkbox);
@@ -62,4 +71,29 @@ const addCheckEventListener = (checkEl) => {
       refreshThePage();
     }, 1000);
   });
+};
+
+const clearInput = () => {
+  inputEl.value = "";
+};
+
+const returnFocus = () => {
+  inputEl.focus();
+};
+
+const processSubmit = () => {
+  console.log("Form submit");
+  const newItem = inputEl.value.trim();
+  const newid = getNextId();
+  let newItemObj = new ToDoItem();
+  newItemObj.setid(newid);
+  newItemObj._item = newItem;
+  todolist.addItem(newItemObj);
+  refreshThePage();
+};
+
+const getNextId = () => {
+  const list = todolist.getList();
+  const nextId = list.length ? list[list.length - 1].id + 1 : 1;
+  return nextId;
 };
